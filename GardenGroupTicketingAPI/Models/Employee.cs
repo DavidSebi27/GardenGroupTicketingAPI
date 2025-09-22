238125dd8e1d74e7e039cb659725806c5534d445
DavidSebi27 <730630@student.inholland.nl>
2025-09-22 13:11:51 +0200

    Employee class created
    Probably needs some revisions after schema update.

--- GardenGroupTicketingAPI/Models/Employee.cs
+++ GardenGroupTicketingAPI/Models/Employee.cs
@@ -1,6 +1,54 @@
-﻿namespace GardenGroupTicketingAPI.Models
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json.Serialization;
+
+namespace GardenGroupTicketingAPI.Models
 {
     public class Employee
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        [BsonElement("first_name")]
+        public string FirstName { get; set; } = null!;
+        [BsonElement("last_name")]
+        public string LastName { get; set; } = null!;
+        [BsonElement("email")]
+        public string Email { get; set; } = null!;
+        [BsonElement("department")]
+        public string Department { get; set; } = null!;
+        [BsonElement("phone_number")]
+        public string? PhoneNumber { get; set; }
+        [BsonElement("address")]
+        public Address? EmployeeAddress { get; set; }
+        [BsonElement("username")]
+        public string Username { get; set; }
+        [BsonElement("password_hash")]
+        [JsonIgnore]
+        public string PasswordHash { get; set; } = null!;
+
+
+
+        public class Address
+        {
+            [BsonElement("street")]
+            public string? Street { get; set; }
+
+            [BsonElement("house_number")]
+            public double? HouseNumber { get; set; }
+
+            [BsonElement("city")]
+            public string? City { get; set; }
+
+            [BsonElement("postal_code")]
+            public string? PostalCode { get; set; }
+        }
+
+        public enum EmployeeRole
+        {
+            RegularEmployee,
+            ServiceDeskEmployee
+        }
     }
 }
